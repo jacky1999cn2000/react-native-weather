f8e23b848161636b9ff72201ec8db02c5763373c
Jacky Zhao <jacky.zhao@bunchball.com>
2016-05-27 22:59:50 -0700

    add geolocation feature

--- README.md
+++ README.md
@@ -1,3 +1,98 @@
 # weather
 
-the use of fetch and MapView
+ * The use of fetch and MapView
+   * MapView has a props called region, and it accepts a value like [longitude, latitude, longitudeDelta, latitudeDelta], where the first 2 parameters determine the center, and the last 2 parameters determine the scale and boundary
+   ```javascript
+   ...
+
+   <MapView
+     region={this.state.mapRegion}
+     annotations={[this.state.pin]}
+     style={styles.map}
+     onRegionChangeComplete={this.onRegionChangeComplete}
+   >
+   </MapView>
+
+   ...
+
+   // MapView regionChangeComplete event handler
+   onRegionChangeComplete = (region) => {
+     let mapRegion = region;
+     mapRegion.longitudeDelta = 5;
+     mapRegion.latitudeDelta = 5;
+
+     this.setState({
+       pin: {
+         longitude: region.longitude,
+         latitude: region.latitude
+       },
+       mapRegion: mapRegion
+     });
+
+     Api(region.latitude, region.longitude)
+       .then((data) => {
+         this.setState(data);
+       })
+   }
+
+   // 'User Current Position' button press event handler
+   _handleStartPress = () => {
+     console.log('pressed@');
+     navigator.geolocation.getCurrentPosition(
+       (position) => {
+         console.log('here');
+         let mapRegion = {
+           longitude: position.coords.longitude,
+           latitude: position.coords.latitude,
+           longitudeDelta: 5,
+           latitudeDelta: 5
+         };
+
+         this.setState({
+           pin: {
+             longitude: position.coords.longitude,
+             latitude: position.coords.latitude
+           },
+           mapRegion: mapRegion
+         });
+       },
+       (error) => {console.log('error here'); console.log(this.state.pin);alert(error.message)},
+       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+     );
+     this.setState({
+       pin: {
+         longitude: 0,
+         latitude: 0
+       },
+     });
+   }
+   ```
+ * [How to use image as background](http://reactcafe.com/how-to-set-a-full-screen-background-image-in-react-native/)
+   * can use any types of images (.jpg .png)
+   * put them in anywhere, and require them as js files
+   * reset image to accommodate the actual real property by `{width: null, height: null}`
+   * set backgroundColor to transparent `backgroundColor: 'rgba(0,0,0,0)'`
+
+   ```javascript
+
+   <Image source={require('./img/flowers.png')} style={[styles.imageContainer]} >
+     <Text style={styles.text}>{this.state.city}</Text>
+     <Text style={styles.text}>{this.state.temperature}</Text>
+     <Text style={styles.text}>{this.state.description}</Text>
+   </Image>
+
+
+   const styles = StyleSheet.create({
+     ...
+     imageContainer: {
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center',
+       width: null,
+       height: null,
+       backgroundColor: 'rgba(0,0,0,0)',
+       resizeMode: 'cover'
+     },
+     ...
+   });
+   ```
--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,9 @@ import {
   StyleSheet,
   MapView,
   Text,
-  View
+  Image,
+  View,
+  TouchableHighlight
 } from 'react-native';
 import Api from './src/api.js';
 
@@ -23,6 +25,7 @@ class weather extends Component {
          latitude: 0,
          longitude: 0
        },
+       mapRegion: null,
        city: '',
        temperature: '',
        description: ''
@@ -30,30 +33,87 @@ class weather extends Component {
   }
 
   render() {
+    console.log('render');
+    console.log('state',this.state.pin);
+    console.log('mapRegion',this.state.mapRegion);
     return (
       <View style={styles.container}>
         <MapView
+          region={this.state.mapRegion}
           annotations={[this.state.pin]}
           style={styles.map}
           onRegionChangeComplete={this.onRegionChangeComplete}
         >
         </MapView>
-        <View style={styles.textWrapper}>
-          <Text style={styles.text}>{this.state.city}</Text>
-          <Text style={styles.text}>{this.state.temperature}</Text>
-          <Text style={styles.text}>{this.state.description}</Text>
-        </View>
+        <Image source={require('./img/flowers.png')} style={[styles.imageContainer]} >
+          <View style={styles.buttonWrapper}>
+            <TouchableHighlight style={styles.touchable}
+
+              onPress={this._handleStartPress}
+            >
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>
+                  User Current Location
+                </Text>
+              </View>
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>{this.state.city}</Text>
+            <Text style={styles.text}>{this.state.temperature}</Text>
+            <Text style={styles.text}>{this.state.description}</Text>
+          </View>
+        </Image>
       </View>
     );
   }
 
-  onRegionChangeComplete = (region) => {
+  _handleStartPress = () => {
+    console.log('pressed@');
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('here');
+        let mapRegion = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+          longitudeDelta: 5,
+          latitudeDelta: 5
+        };
+
+        this.setState({
+          pin: {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          },
+          mapRegion: mapRegion
+        });
+      },
+      (error) => {console.log('error here'); console.log(this.state.pin);alert(error.message)},
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
     this.setState({
       pin: {
-        longitude: region.longitude,
-        latitude: region.latitude
-      }
+        longitude: 0,
+        latitude: 0
+      },
     });
+  }
+
+  onRegionChangeComplete = (region) => {
+    console.log('onRegionChangeComplete');
+    console.log('region',region.longitude,region.latitude);
+    // let mapRegion = region;
+    // mapRegion.longitudeDelta = 5;
+    // mapRegion.latitudeDelta = 5;
+    //
+    // this.setState({
+    //   pin: {
+    //     longitude: region.longitude,
+    //     latitude: region.latitude
+    //   },
+    //   mapRegion: mapRegion
+    // });
 
     Api(region.latitude, region.longitude)
       .then((data) => {
@@ -73,12 +133,43 @@ const styles = StyleSheet.create({
     flex: 2,
     marginTop: 30
   },
-  textWrapper: {
+  imageContainer: {
     flex: 1,
+    width: null,
+    height: null,
+    backgroundColor: 'rgba(0,0,0,0)',
+    resizeMode: 'cover'
+  },
+  buttonWrapper: {
+    flex: 2,
+    justifyContent: 'center',
     alignItems: 'center'
   },
+  touchable: {
+    borderRadius: 50
+  },
+  button: {
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 50
+  },
+  textWrapper: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    opacity: 0.3,
+  },
   text: {
-    fontSize: 30
+    fontSize: 20,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'white'
   }
 });
 
